25d10c08fe407ff2735b98d20a49aaf05fc67b13
Peter van Heusden <pvh@sanbi.ac.za>
2021-01-08 16:19:30 +0200

    Remove equals sign

--- tb-sample-report-pipeline-plugin/src/main/java/ca/corefacility/bioinformatics/irida/plugins/SnippyPluginUpdater.java
+++ tb-sample-report-pipeline-plugin/src/main/java/ca/corefacility/bioinformatics/irida/plugins/SnippyPluginUpdater.java
@@ -229,7 +229,7 @@ public class SnippyPluginUpdater implements AnalysisSampleUpdater {
 				Map.Entry("ethambutol_variants", new MetadataValue("Ethambutol Res Variants", "")),
 				Map.Entry("streptomycin_variants", new MetadataValue("Streptomycin Res Variants", "")),
 				Map.Entry("other_resistance_variants", new MetadataValue("Other resistance Res Variants", ""));
-=			// @formatter:on				
+			// @formatter:on				
 
 			// get the last (i.e. most specific) lineage entry
 			Lineage lineage = tbprofilerResults.lineage.get(tbprofilerResults.lineage.size() - 1);
