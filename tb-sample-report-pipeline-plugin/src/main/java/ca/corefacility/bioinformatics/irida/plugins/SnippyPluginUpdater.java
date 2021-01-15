d8b52a8006a908cb0d5efaf53c9be7a8c4e5e2f7
Peter van Heusden <pvh@sanbi.ac.za>
2021-01-15 20:39:22 +0200

    Fix float percentage formatting

--- tb-sample-report-pipeline-plugin/src/main/java/ca/corefacility/bioinformatics/irida/plugins/SnippyPluginUpdater.java
+++ tb-sample-report-pipeline-plugin/src/main/java/ca/corefacility/bioinformatics/irida/plugins/SnippyPluginUpdater.java
@@ -301,7 +301,7 @@ public class SnippyPluginUpdater implements AnalysisSampleUpdater {
 			}
 			
 			Map<String, MetadataEntry> metadataEntries = new HashMap<>();
-			String mappingPercentage = String.format(".2%f %% %f %f", mappedSequences / totalGoodSequences, mappedSequences, totalGoodSequences);
+			String mappingPercentage = String.format("%.2f %% %f %f", mappedSequences / totalGoodSequences, mappedSequences, totalGoodSequences);
 			PipelineProvidedMetadataEntry metadataEntry = new PipelineProvidedMetadataEntry(mappingPercentage, "text", analysis);
 			metadataEntries.put("a. % Reads Mapped", metadataEntry);
 
